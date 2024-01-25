38a2fad30cf9c262e9a679214c69b2a610803328
Michael Hernando <mhernando334@gmail.com>
2024-01-25 12:10:18 -0500

    project initialization

---
+++ app/src/main/java/com/example/simplecounter/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.simplecounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        val upgradeBtn = findViewById<Button>(R.id.button2)
+
+        button.setOnClickListener {
+//            Toast.makeText(it.context, "Clicked button!", Toast.LENGTH_SHORT).show()
+            counter++
+            textView.text = counter.toString()
+
+            if (counter >= 100) {
+                upgradeBtn.visibility = View.VISIBLE
+                upgradeBtn.setOnClickListener {
+                    button.text = "Add 2"
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+                    upgradeBtn.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+}
